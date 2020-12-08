1a7bffa9ad509421c8bb101db06461db76d70661
Mathieu Letendre-Jauniaux <Mathieu.Letendre-Jauniaux@agilebits.com>
2020-12-07 22:15:15 -0500

    Clean up error.

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,93 +1,121 @@
+use displaydoc::Display;
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    convert::TryFrom,
+};
+use thiserror::Error;
 
-pub fn challenge1(input: &str) -> usize {
-    let re = Regex::new(r"[[:digit:]]+ (?P<color>[[:alpha:]]+ [[:alpha:]]+) bag").unwrap();
+struct Rule<'a> {
+    bag: &'a str,
+    contents: HashMap<&'a str, u32>,
+}
 
-    let containing: HashMap<&str, HashSet<&str>> = input
-        .lines()
-        .flat_map(
-            |line| match line.split(" bags contain ").collect::<Vec<_>>()[..] {
-                [bag, contains] => re
+#[derive(Debug, Error, Display)]
+pub enum Errors {
+    /// Parse error {0}
+    ParseError(#[from] std::num::ParseIntError),
+    /// Bad line {0}
+    BadLine(String),
+    /// Bad regex {0}
+    BadRegex(#[from] regex::Error),
+    /// Missing placeholder {0}
+    MissingPlaceholder(&'static str),
+    /// Bag bag {0}
+    UnknownBag(String),
+}
+
+impl<'a> TryFrom<&'a str> for Rule<'a> {
+    type Error = Errors;
+
+    fn try_from(line: &'a str) -> Result<Self, Self::Error> {
+        let re = Regex::new(r"(?P<count>[[:digit:]]+) (?P<color>[[:alpha:]]+ [[:alpha:]]+) bag")?;
+
+        match line.split(" bags contain ").collect::<Vec<_>>()[..] {
+            [bag, contains] => {
+                let contents = re
                     .captures_iter(contains)
-                    .map(move |captures| (captures.name("color").unwrap().as_str(), bag)),
-                _ => panic!("Line doesnt match the expected pattern {}", line),
-            },
-        )
-        .fold(HashMap::new(), |mut acc, (contained, contained_by)| {
-            acc.entry(contained)
-                .or_insert_with(HashSet::new)
-                .insert(contained_by);
+                    .map(|captures| {
+                        Ok((
+                            captures
+                                .name("color")
+                                .ok_or(Errors::MissingPlaceholder("color"))?
+                                .as_str(),
+                            captures
+                                .name("count")
+                                .ok_or(Errors::MissingPlaceholder("count"))?
+                                .as_str()
+                                .parse()?,
+                        ))
+                    })
+                    .collect::<Result<_, Errors>>()?;
+
+                Ok(Rule { bag, contents })
+            }
+            _ => Err(Errors::BadLine(line.to_string())),
+        }
+    }
+}
+
+pub fn challenge1(input: &str) -> Result<usize, Errors> {
+    let rules = input
+        .lines()
+        .map(Rule::try_from)
+        .collect::<Result<Vec<Rule>, _>>()?;
+
+    let containing: HashMap<&str, HashSet<&str>> =
+        rules.into_iter().fold(HashMap::new(), |mut acc, rule| {
+            let Rule { bag, contents } = rule;
+            contents.into_iter().for_each(|(contained, _count)| {
+                acc.entry(contained)
+                    .or_insert_with(HashSet::new)
+                    .insert(bag);
+            });
             acc
         });
 
-    recursive_container(&containing, "shiny gold").len()
-}
-
-fn recursive_container<'m>(
-    map: &'m HashMap<&str, HashSet<&str>>,
-    target: &str,
-) -> HashSet<&'m str> {
-    if let Some(bags) = map.get(target) {
-        let mut union = bags.clone();
-        for bag in bags.iter() {
-            union.extend(recursive_container(map, bag));
-        }
-        union
-    } else {
-        HashSet::new()
+    fn recurse<'m>(map: &'m HashMap<&str, HashSet<&str>>, target: &str) -> HashSet<&'m str> {
+        map.get(target)
+            .into_iter()
+            .flat_map(|bags| {
+                bags.iter()
+                    .copied()
+                    .chain(bags.iter().flat_map(|bag| recurse(map, bag)))
+            })
+            .collect()
     }
+
+    Ok(recurse(&containing, "shiny gold").len())
 }
 
-pub fn challenge2(input: &str) -> u32 {
-    let re =
-        Regex::new(r"(?P<count>[[:digit:]]+) (?P<color>[[:alpha:]]+ [[:alpha:]]+) bag").unwrap();
-
-    let container: HashMap<&str, HashMap<&str, u32>> = input
+pub fn challenge2(input: &str) -> Result<u32, Errors> {
+    let index: HashMap<&str, Rule> = input
         .lines()
-        .map(
-            |line| match line.split(" bags contain ").collect::<Vec<_>>()[..] {
-                [bag, contains] => (
-                    bag,
-                    re.captures_iter(contains)
-                        .map(|captures| {
-                            (
-                                captures.name("color").unwrap().as_str(),
-                                captures.name("count").unwrap().as_str().parse().unwrap(),
-                            )
-                        })
-                        .collect(),
-                ),
-                _ => panic!("Line doesn't match the expected pattern {}", line),
-            },
-        )
-        .collect();
+        .map(Rule::try_from)
+        .map(|result| result.map(|rule| (rule.bag, rule)))
+        .collect::<Result<_, _>>()?;
 
-    recursive_containing(&container, "shiny gold", 1)
-}
+    fn recurse<'m>(index: &'m HashMap<&str, Rule>, target: &str) -> Result<u32, Errors> {
+        let rule = index
+            .get(target)
+            .ok_or_else(|| Errors::UnknownBag(target.to_string()))?;
 
-fn recursive_containing<'m>(
-    map: &'m HashMap<&str, HashMap<&str, u32>>,
-    target: & str,
-    factor: u32,
-) -> u32 {
-    if let Some(bags) = map.get(target) {
-        let mut total_count = 0;
-        
-        for (bag, count) in bags.iter() {
-            total_count += count * factor + recursive_containing(map, bag, factor * count);
+        let mut sum = 0;
+        for (bag, count) in rule.contents.iter() {
+            sum += count + count * recurse(index, bag)?;
         }
-        total_count
-    } else {
-        panic!("Unknown bag {}", target);
+
+        Ok(sum)
     }
+
+    recurse(&index, "shiny gold")
 }
 
 #[cfg(test)]
 mod tests {
 
     #[test]
-    fn test_challenge1() {
+    fn test_challenge1() -> Result<(), super::Errors> {
         const INPUT: &str = r#"light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 bright white bags contain 1 shiny gold bag.
@@ -98,11 +126,12 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags."#;
 
-        assert_eq!(super::challenge1(INPUT), 4);
+        assert_eq!(super::challenge1(INPUT)?, 4);
+        Ok(())
     }
 
     #[test]
-    fn test_challenge2() {
+    fn test_challenge2() -> Result<(), super::Errors> {
         const INPUT: &str = r#"shiny gold bags contain 2 dark red bags.
 dark red bags contain 2 dark orange bags.
 dark orange bags contain 2 dark yellow bags.
@@ -110,6 +139,7 @@ dark yellow bags contain 2 dark green bags.
 dark green bags contain 2 dark blue bags.
 dark blue bags contain 2 dark violet bags.
 dark violet bags contain no other bags."#;
-        assert_eq!(super::challenge2(INPUT), 126);
+        assert_eq!(super::challenge2(INPUT)?, 126);
+        Ok(())
     }
 }
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,6 +1,8 @@
-fn main() {
+fn main() -> Result<(), day7::Errors> {
     let input = include_str!("input.txt");
-    println!("Challenge 1: {}", day7::challenge1(input));
+    println!("Challenge 1: {}", day7::challenge1(input)?);
 
-    println!("Challenge 2: {}", day7::challenge2(input));
+    println!("Challenge 2: {}", day7::challenge2(input)?);
+
+    Ok(())
 }
